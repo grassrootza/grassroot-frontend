3816a4cd21ba2afcd356140660a51a8697a45949
Igor Buzatovic <beegor@gmail.com>
2017-09-22 08:39:04 +0200

    auth.service cleanup fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {AuthService} from "./auth.service";
 import {UserService} from "./user/user.service";
 
 
--- src/app/logged-in.guard.ts
+++ src/app/logged-in.guard.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
 import {Observable} from 'rxjs/Observable';
-import {AuthService} from './auth.service';
 import {UserService} from "./user/user.service";
 
 @Injectable()
