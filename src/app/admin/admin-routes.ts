997e67f60bd0f5a23e38d17ad5f153bd36067085
Luke Jordan <luke.jordan@grassroot.org.za>
2018-04-12 14:42:41 +0200

    WIP started on role security addition

--- src/app/admin/admin-routes.ts
+++ src/app/admin/admin-routes.ts
@@ -2,8 +2,11 @@ import {Routes} from "@angular/router";
 import {LoggedInGuard} from "../logged-in.guard";
 import {LiveWireListComponent} from "./livewire/alert-list/live-wire-list.component";
 import {ViewAlertComponent} from "./livewire/alert-view/view-alert.component";
+import {AdminRoleGuard} from "./admin-role.guard";
 
 export const ADMIN_ROUTES : Routes = [
-    {path: 'livewire/list', component: LiveWireListComponent, canActivate: [LoggedInGuard]},
-    {path: 'livewire/:id', component: ViewAlertComponent, canActivate: [LoggedInGuard]},
+    {path: 'livewire/list', component: LiveWireListComponent, canActivate: [LoggedInGuard, AdminRoleGuard],
+      data: {roles: ['ROLE_LIVEWIRE_USER']}},
+    {path: 'livewire/:id', component: ViewAlertComponent, canActivate: [LoggedInGuard, AdminRoleGuard],
+      data: {roles: ['ROLE_LIVEWIRE_USER']}},
   ];
