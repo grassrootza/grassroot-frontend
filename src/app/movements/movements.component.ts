740504923309c2bd32d1e24e05581022a66b490a
Luke Jordan <luke.jordan@grassroot.org.za>
2018-06-12 18:52:21 +0200

    Stubbed out movements service, module, etc

---
+++ src/app/movements/movements.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movements',
+  templateUrl: './movements.component.html',
+  styleUrls: ['./movements.component.css']
+})
+export class MovementsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
